3343da94acb64ff8ab0aeb9dee6f90efbcafa17f
Claud Bozga <claudia.bozga@gmail.com>
2023-10-15 17:36:15 +0300

    adaugat produse pipeline

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML += `
-  <div>
-    <h3>Cla</h3>
-    <img id="strawbs" src="https://clv.h-cdn.co/assets/15/22/2560x1728/gallery-1432664914-strawberry-facts1.jpg" />
-  </div>
-`
+window.addEventListener('load', showProducts);
+
+async function showProducts() 
+{
+  console.log('Page fully loaded');
+  var response = await fetch (
+  'https://652bdb78d0d1df5273eecec8.mockapi.io/products');
+  var products = await response.json();
+  // console.log(products);
+
+  var productsDiv = document.getElementById('products')
+  var productsHTML = "";
+
+  for (var i=1; i<=products.length - 1; i++)
+  {
+    productsHTML += `<div class="card">
+      <img src=${products[i].imageURL}/>
+    </div>`;
+  }
+  productsDiv.innerHTML = productsHTML;
+}
