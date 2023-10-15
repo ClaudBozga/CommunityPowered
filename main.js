b51212a513fde0e05eca65ded3c9ce2129c67ff7
Claud Bozga <claudia.bozga@gmail.com>
2023-10-15 14:27:01 +0300

    pagini web

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
+document.querySelector('#app').innerHTML += `
   <div>
     <h3>Cla</h3>
-    <img src="https://clv.h-cdn.co/assets/15/22/2560x1728/gallery-1432664914-strawberry-facts1.jpg" />
+    <img id="strawbs" src="https://clv.h-cdn.co/assets/15/22/2560x1728/gallery-1432664914-strawberry-facts1.jpg" />
   </div>
 `
